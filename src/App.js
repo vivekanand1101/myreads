8fbbfe4d76bb7a505341f7dd710f4799123d5607
Vivek Anand <vivekanand1101@gmail.com>
2018-11-29 01:19:20 +0530

    Add main page and search page
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import PropTypes from 'prop-types'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import './App.css'
 
 
 class SearchComponenet extends React.Component {
+
+  static propTypes = {
+    moveBook: PropTypes.func.isRequired,
+  }
+  state = {
+    query: '',
+    books: [],
+  }
+  updateSearchBox = (event) => {
+    const value = event.target.value
+    this.setState({
+      query: value,
+      books: []
+    })
+    this.searchBooks(value)
+  }
+  searchBooks = (value) => {
+    if (value.length === 0) {
+      this.setState({
+        query: value,
+        books: []
+      })
+    } else {
+      BooksAPI.search(value, 10).then((books) => {
+        if (books.length > 0) {
+          this.setState({
+            query: value,
+            books: books.filter((book) => book.imageLinks !== undefined && book.imageLinks.thumbnail !== undefined),
+          })
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+          <Link to='/'><button className="close-search">Close</button></Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input type="text" onChange = {this.updateSearchBox} value= {this.state.query} placeholder="Search by title or author"/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+          {this.state.books.length > 0 && this.state.books.map((book, index) => (<Book key={index} book={book} moveBook={(to, book) => this.props.moveBook(to, book)}/>))}
+          </ol>
         </div>
       </div>
     )
@@ -34,14 +61,27 @@ class SearchComponenet extends React.Component {
 
 
 class Book extends React.Component {
+
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    moveBook: PropTypes.func.isRequired,
+  }
+
+  moveBook = (event, book) => {
+    if (this.props.moveBook) {
+      this.props.moveBook(event.target.value, book)
+    }
+  }
   render() {
     const book = this.props.book
+    const thumbnail = book.imageLinks.thumbnail
     return (
+      <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.url}}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + thumbnail + ')'}}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select onChange={(event) => this.moveBook(event, book)} value={book.shelf}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -50,23 +90,29 @@ class Book extends React.Component {
               </select>
             </div>
           </div>
-          <div className="book-title">{book.name}</div>
-          <div className="book-authors">{book.author}</div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.hasOwnProperty('authors') && book.authors.length > 0 && book.authors[0]}</div>
         </div>
+      </li>
     )
   }
 }
 
 
 class Shelf extends React.Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    moveBook: PropTypes.func.isRequired,
+    readStatus: PropTypes.string.isRequired,
+  }
   render() {
-    const {books, readStatus} = this.props
+    const {books, readStatus, moveBook} = this.props
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{readStatus}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {books.map(book => <li><Book book={book} /> </li> )}
+            {books.map((book, index) => <Book key={index} book={book} moveBook={(to, book) => moveBook(to, book)}/>)}
           </ol>
         </div>
       </div>
@@ -76,36 +122,19 @@ class Shelf extends React.Component {
 
 
 class ListBooks extends React.Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  }
+
+  moveBook = (to, book) => {
+    this.props.moveBook(to, book)
+  }
   render() {
+    const currentlyReadingBooks = this.props.books.filter((book) => book.shelf === 'currentlyReading')
+    const wantToReadBooks = this.props.books.filter((book) => book.shelf === 'wantToRead')
+    const readBooks = this.props.books.filter((book) => book.shelf === 'read')
 
-    const currentlyReading = [{
-      'url': 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' ,
-      'name': 'To Kill a Mockingbird',
-      'author': 'Harper Lee',
-    }, {
-      'url': 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' ,
-      'name': "Ender's Game",
-      'author': 'Orson Scott Card',
-    }]
-    const wantToRead = [{
-      'url': 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' ,
-      'name': "Harry Potter and the Sorcerer's Stone",
-      'author': 'J.K. Rowling',
-    }, {
-      'url': 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' ,
-      'name': '1776',
-      'author': 'David McCullough',
-    }]
-
-    const read = [{
-      'url': 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' ,
-      'name': "The Hobbit",
-      'author': 'J.R.R. Tolkien',
-    }, {
-      'url': 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' ,
-      'name': "Oh, the Places You'll Go!",
-      'author': 'Seuss',
-    }]
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -115,50 +144,71 @@ class ListBooks extends React.Component {
           <div>
             <Shelf
             readStatus='Currently Reading'
-            books={currentlyReading}
+            books={currentlyReadingBooks}
+            moveBook={this.moveBook}
             />
             <Shelf
             readStatus='Want to Read'
-            books={wantToRead}
+            books={wantToReadBooks}
+            moveBook={this.moveBook}
             />
             <Shelf
             readStatus='Read'
-            books={read}
+            books={readBooks}
+            moveBook={this.moveBook}
             />
           </div>
         </div>
-        <div className="open-search">
-          <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-        </div>
+        <SearchLink/>
       </div>
-
     )
   }
 }
 
-class BooksApp extends React.Component {
-  static propTypes =  {
-    // somthing here
+
+class SearchLink extends React.Component {
+  render() {
+    return (
+      <div className="open-search">
+        <Link to='/search'><button>Add a book</button></Link>
+      </div>
+    )
   }
+}
+
+
+class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    this.fetchAllBooks()
+  }
+
+  fetchAllBooks = () => {
+    BooksAPI.getAll().then(
+      (books) => (
+        this.setState({books: books})
+      )
+    )
+  }
+
+  moveBook = (to, book) => {
+    BooksAPI.update(book, to).then(() => {
+      this.fetchAllBooks()
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() =>
-          <ListBooks/>
+          <ListBooks books={this.state.books} moveBook={this.moveBook}/>
         }
         />
-        <Route path='/search' render={() =>
-          <SearchComponenet/>
+        <Route exact path='/search' render={() =>
+          <SearchComponenet  moveBook={this.moveBook}/>
         }
         />
       </div>
